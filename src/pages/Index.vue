bc4a039d8ac706acb76322cbbe72994573fb5da8
Sebastian Scholl <sebscholl@gmail.com>
2020-07-20 13:14:14 -0400

    changes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,13 +3,22 @@
     <img class="profile-image" src="../../static/Profile.png" />
 
     <h2 class="my-4 text-intro">Hi, I'm Sebastian.</h2>
-    
+
     <p>
-      There's not much to see here! If you want to connect, use one of the platforms linked below.
+      There's not much to see here...
     </p>
 
     <p>
-      Otherwise... <a href="https://www.google.com/search?q=Sebastian+Scholl" class="google-link">try this?</a>
+      If you want to connect, use one of the platforms linked below.
+    </p>
+
+    <p>
+      Otherwise...
+      <a
+        href="https://www.google.com/search?q=Sebastian+Scholl"
+        class="google-link"
+        >try this?</a
+      >
     </p>
   </Layout>
 </template>
@@ -17,12 +26,16 @@
 <script>
 export default {
   metaInfo: {
-    title: "Home page | Sebastian Scholl professional site"
-  }
+    title: "Home page | Sebastian Scholl professional site",
+  },
 };
 </script>
 
 <style>
+* {
+  text-align: center;
+}
+
 .profile-image {
   display: block;
   margin: 0 auto;
